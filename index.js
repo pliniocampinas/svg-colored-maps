4ecbf54fdfcadfdfd56a367c5a29dd39a6038f7c
Plinioc <plinio.lsc@gmail.com>
2023-12-01 01:13:29 -0300

    First fix

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { StatesMapBuilder } = require('./map-tools/brazil-states-tools.js')
 const { SaoPauloMunicipalitiesMapBuilder } = require('./map-tools/sao-paulo-municipalities-tools.js')
 const mapBuildingTools = require('./map-tools/map-building-tools.js')
 
-module.exports = MunicipalitiesMapBuilder
-module.exports = StatesMapBuilder
-module.exports = SaoPauloMunicipalitiesMapBuilder
-module.exports = mapBuildingTools
+module.exports = {
+  MunicipalitiesMapBuilder,
+  StatesMapBuilder,
+  SaoPauloMunicipalitiesMapBuilder,
+  mapBuildingTools,
+}
